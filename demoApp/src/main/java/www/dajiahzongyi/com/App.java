765b33e7fb60dd89208a4d18533a737070f43e28
虞天健 <gutakashiken@yutianjiandeMacBook-Pro.local>
2019-01-29 15:27:28 +0800

    update

--- demoApp/src/main/java/www/dajiahzongyi/com/App.java
+++ demoApp/src/main/java/www/dajiahzongyi/com/App.java
@@ -27,6 +27,10 @@ class MyThread extends Thread {
     }
     public void run() {
         while (formCount >= 0) {
+            // 同一个线程可能只执行当前方法中的1句（当前执行栈中的一小部分）
+            System.out.println(Thread.currentThread().getName() + ":步骤1");
+            System.out.println(Thread.currentThread().getName() + ":步骤2");
+            System.out.println(Thread.currentThread().getName() + ":步骤3");
             System.out.println(Thread.currentThread().getName() + "正在分发：" + formCount--); // 线程不安全
         }
     }
